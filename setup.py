fc81a5c044d0912e51854529bc16c2678559a49a
dave <dave@davepedu.com>
2017-05-02 20:34:12 -0700

    Correct repo url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from findbig import __version__
 setup(name='findbig',
       version=__version__,
       description='Command-line tool for finding the biggest files in a directory tree',
-      url='http://gitlab.davepedu.com/dave/findbog',
+      url='http://gitlab.davepedu.com/dave/findbig',
       author='dpedu',
       author_email='dave@davepedu.com',
       packages=['findbig'],
